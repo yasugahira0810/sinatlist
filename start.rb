61151fd47790199a803891072dc9d6edadef6aab
Yuta Yasugahira <hourou_freak@yahoo.co.jp>
2015-10-25 00:40:31 +0000

    implement insert function

--- start.rb
+++ start.rb
@@ -17,3 +17,12 @@ get '/' do
   @students = Student.all
   erb :index
 end
+
+post '/new' do
+  student = Student.new
+  student.id = params[:id]
+  student.name = params[:name]
+  student.email = params[:email]
+  student.save
+  redirect '/'
+end
